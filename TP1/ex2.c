e042a35997b0745ffc70a72f6b96a38b150765cf
luckydu43 <luc.duperron@laposte.net>
2020-10-30 21:17:53 +0100

    UPT(smb101) : indentations ex2

--- TP1/ex2.c
+++ TP1/ex2.c
@@ -6,32 +6,31 @@
 #include <sys/wait.h>
 
 /*TD 30102020 : Ex.2 :OK*/
-int main(void)
-{
-int p; int d1; int d2; char r[30];
-p = fork();
+int main(void) {
+	int p;
+	int d1;
+	int d2;
+	char r[30];
 
+	p = fork();
+	if(p == 0) {
+		d1=open("file.txt", O_WRONLY);
+		write(d1, "je suis eleve du Cnam", 21);
+		close(d1);
+	}
 
- if(p == 0) {
-d1=open("file.txt", O_WRONLY);
-write(d1, "je suis eleve du Cnam", 21) ;
-close(d1) ;
-}
+	if(p > 0) {
+		wait(NULL);
+		d2=open("file.txt", O_RDONLY);
+		read(d2, &r, 21);
+		printf("info lue :%s\n", r);
+		close(d2);
+	}
 
+	if(p < 0) {
+		printf("Probleme de creation par fork()\n");
+	}
 
- if(p > 0)
-{
-wait(NULL);
-d2=open("file.txt", O_RDONLY);
-read(d2, &r, 21) ;
-printf("info lue :%s\n", r) ;
-close(d2) ;
-}
-
-
-if(p < 0)printf("Probleme de creation par fork()\n");
-
-
- return 0;
+	return 0;
 }
 
